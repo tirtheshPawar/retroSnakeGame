fe406db14951429ce43d18eb0162ee600687ba16
tirtheshPawar <pawartirthesh3@gmail.com>
2024-06-08 18:56:39 -0300

    Resolved rapid speed increase bug (when snake eats food), added highscore functionality, checks for collision are in place, reset game and stop game functionality

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 const board = document.getElementById("game-board");
 const instructionText = document.getElementById("instruction-text");
 const logo = document.getElementById('logo');
-
+const score = document.getElementById('score')
+const highScoreText= document.getElementById('highScore')
 // Defining game variables
 let snake = [{x: 10, y:10}]; //object stored inside array, x&y coords defining 10,10 middle of the board starting position
 const gridSize = 20;
 let food = generateFood();
+let highScore = 0;
 let direction = 'right';
 let gameInterval;
 let gameSpeedDelay = 200;
 let gameStarted = false;
 
 
-// Draws game map, snake and food
+// Draws game map, snake and food, update score
 function draw(){
     board.innerHTML = '';
     drawSnake();
     drawFood();
+    updateScore();
 }
 
 // Creates the snake on the board 
@@ -48,9 +51,11 @@ function setPosition(element, position){
 
 // Draw food
 function drawFood(){
+    if (gameStarted){
     const foodElement = createGameElement('div','food');
     setPosition(foodElement, food)
     board.appendChild(foodElement);
+}
 }
 
 // Generates food on the map on a random position every time
@@ -83,9 +88,11 @@ function move(){
     // snake.pop();
     if(head.x === food.x && head.y === food.y){
         food = generateFood(); // Since we have run into the food we need new food on the map now
-        clearInterval(); //Resetting movement (move) function
+        increaseSpeed();
+        clearInterval(gameInterval); //Resetting movement (move) function
         gameInterval = setInterval(()=>{
             move();
+            checkCollision();
             draw();
         },gameSpeedDelay);
     }else{
@@ -105,7 +112,7 @@ function startGame(){
     logo.style.display = 'none';
     gameInterval = setInterval(()=>{
         move();
-        // checkCollision();
+        checkCollision();
         draw();
     }, gameSpeedDelay);
 }
@@ -136,3 +143,66 @@ function handleKeyPress(event){
 }
 
 document.addEventListener('keydown',handleKeyPress)
+
+function increaseSpeed(){
+// console.log(gameSpeedDelay);
+if(gameSpeedDelay>150){
+gameSpeedDelay -= 5;
+}
+else if (gameSpeedDelay > 100) {
+gameSpeedDelay -= 3; //decreases the speed getting faster
+}
+else if (gameSpeedDelay > 50) {
+gameSpeedDelay -= 2; //decreases the speed getting faster
+}
+else if (gameSpeedDelay > 25) {
+gameSpeedDelay -= 1; //decreases the speed getting faster
+}
+}
+
+function checkCollision(){
+    const head = snake[0];
+
+    // setting boundaries for the snake using coordinates and gridsize
+    if(head.x <1 || head.x > gridSize || head.y < 1 || head.y > gridSize){
+        resetGame();
+    }
+    // Checking if the snake hits itself
+    for (let i =1; i<snake.length; i++){
+        // if the head is in the same position as the body in the ith position
+        if(head.x === snake[i].x && head.y === snake[i].y){
+            resetGame();
+        }
+    }
+}
+
+function resetGame(){
+    updateHighScore();
+    stopGame();
+    snake = [{x:10, y:10}];
+    food = generateFood();
+    direction = 'right';
+    gameSpeedDelay = 200;
+    updateScore();
+}
+
+function updateScore(){
+    const currentScore = snake.length - 1;
+    score.textContent = currentScore.toString().padStart(3,'0'); //adds padding of zeroes if the score is less than 3 digits
+}
+
+function stopGame(){
+    clearInterval(gameInterval);
+    gameStarted = false;
+    instructionText.style.display = 'block'; //since it was hidden before
+    logo.style.display = 'block'; //since it was hidden before
+}
+
+function updateHighScore(){
+    const currentScore = snake.length -1;
+    if(currentScore > highScore){
+        highScore = currentScore;
+        highScoreText.textContent = highScore.toString().padStart(3,0);
+    }
+    highScoreText.style.display='block';
+}
